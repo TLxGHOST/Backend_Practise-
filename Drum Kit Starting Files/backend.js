6fbdf0f7de9b8bec1de3c4262836ab9a16563a96
Scorpion <btejanshu@gmail.com>
2026-03-13 23:47:31 +0530

    remove all node_modules from repository

--- Drum Kit Starting Files/backend.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-
-const app=express();
-app.get("/",(req,res)=>{
-  res.send("./")
-})
-
-app.listen(3000,()=>{console.log("Server is live ");});
--- Part3(Axios)/public/api.js
+++
@@ -1,7 +0,0 @@
-import axios from "https://cdn.jsdelivr.net/npm/axios@1.6.8/+esm";
-
-const api = axios.create({
-  baseURL: "http://localhost:5500"
-});
-
-export default api;
